0dab3eb1c828f941d56f35eb904165298b9bb950
Samuel Valentin <samuelv.lopez@iteso.mx>
2023-09-27 00:28:27 -0600

    it works and has time execution

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <math.h>
-#define n 10
+#include <time.h>
+
+#define n 999999
 #define upper 1
 #define lower 0
 
 double area (double );
-double pi_value (double );
+double pi_value ();
 double fx (double);
 
 
-double pi_value(double value){
+double pi_value(){
     double base = (upper - lower) / (double)n;      //Calculo de la base, en realidad se calcula la distancia entre los centros de los rectangulos, pero esto es lo mimso que la base
     double suma = 0;
 
@@ -20,19 +21,23 @@ double pi_value(double value){
     return suma;
 }
 
-double area(double x){
-    double value = fx(x);
-    return pi_value(value);
-}
-
 double fx (double x) {
     return 4.0 / (1.0+(x*x));     //Funcion para calcular pi
 }
 
 int main() {
-    double pi = area(n);
+    clock_t inicio, fin;
+    double tiempo;
+    inicio = clock();       // Inicio del temporizador
 
-    printf("El valor de PI con %d intervalo es: %.20f", n,pi);
+    double pi = pi_value();
+    printf("El valor de PI con %d intervalo es: %.20f\n", n,pi);
+
+    fin = clock();      // Fin del temporizador
+
+    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;           // Calcular el tiempo transcurrido en segundos
+
+    printf("Tiempo de ejecucion: %.4fs\n", tiempo);
 
     return 0;
 }
